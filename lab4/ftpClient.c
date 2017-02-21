efe6929c826186e34f9d46437fe266ab7cb37a5e
Ray <kinsella222@gmail.com>
2017-02-21 19:20:26 +0000

    Documentation

--- lab4/ftpClient.c
+++ lab4/ftpClient.c
@@ -11,6 +11,7 @@ int main(int argc , char *argv[]){
     struct sockaddr_in server;
     char message[32] , server_reply[1024], quit[5], getCheck[4], *file, filename[32];
 
+    //Verify IP was provided
     if(argc != 2){
       printf("Usage: Server IP address");
       return 1;
@@ -22,55 +23,63 @@ int main(int argc , char *argv[]){
         perror("Could not create socket");
     }
 
+    //Set up server object
     server.sin_addr.s_addr = inet_addr(argv[1]);
     server.sin_family = AF_INET;
     server.sin_port = htons(5000);
 
-    //Connect to remote server
+    //Connect to server
     if(connect(sock ,(struct sockaddr *)&server , sizeof(server)) < 0){
         perror("connect failed. Error");
         return 1;
     }
 
-    //keep communicating with server
     do{
+        //clear message and reply arrays
         memset(server_reply, 0, sizeof(server_reply));
         memset(message, 0, sizeof(message));
 
+        //Accept user choice and parse accordingly
         printf("ftp> ");
         fgets(message, 32, stdin);
         strncpy(quit, message, 4);
         quit[4] = '\0';
 
-        //Send some data
+        //Send command to server
         if(send(sock , message , strlen(message) , 0) < 0){
             perror("Send failed");
             return 1;
         }
 
+        //Seperate pathing if user uses get command
         strncpy(getCheck, message, 3);
         getCheck[3] = '\0';
         if(strcmp(getCheck, "get") == 0){
 
+            //Get file size to allocate memory
             if(recv(sock, &size, sizeof(int), 0) < 0){
                 perror("recv failed");
                 break;
             }
 
+            //If size is zero, either file does not exist or is directory
             if(size == 0){
                 printf("No such file in the directory\n");
                 continue;
             }
+
+            //Get filename for creation after download, and allocate file memory
             recv(sock, filename, sizeof(filename), 0);
             file = malloc(size);
             recv(sock, file, size, 0);
             filehandle = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
 
+            //write data from server into file and close file.
             write(filehandle, file, size);
             close(filehandle);
             printf("File: %s recieved\n", filename);
         }else{
-            //Receive a reply from the server
+            //Receive information per request from the server
             if(recv(sock , server_reply , sizeof(server_reply) , 0) < 0){
                 perror("recv failed\n");
                 break;
--- lab4/ftpServer.c
+++ lab4/ftpServer.c
@@ -23,20 +23,19 @@ int main(int argc , char *argv[])
         printf("Could not create connect socket");
     }
 
-    //Prepare the sockaddr_in structure
+    //Prepare the server object
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(5000);
 
+    //Bind server to socket
     if(bind(connect_fd,(struct sockaddr *)&server , sizeof(server)) < 0){
-        //print the error message
         perror("bind failed. Error");
         return 1;
     }
 
+    //listen over connection socket for client.
     listen(connect_fd , 3);
-
-    //Accept and incoming connection
     c = sizeof(struct sockaddr_in);
 
     //accept connection from an incoming client
@@ -47,12 +46,17 @@ int main(int argc , char *argv[])
     }
 
     do{
+      //clear recieve buf and command array
       memset(buf, 0, sizeof(buf));
       memset(command, 0, sizeof(command));
+
+      //Listen for client query
 			read_size = recv(client_fd , buf, sizeof(buf), 0);
 
+      //Parse command type
       sscanf(buf, "%s", command);
 
+      //If ls or dir, return current directory contents.
 			if(strcmp(command, "ls") == 0 || strcmp(command, "dir") == 0){
             d = opendir(".");
             sprintf(out, "\nContents of Directory:\n");
@@ -67,10 +71,15 @@ int main(int argc , char *argv[])
 						write(client_fd , out , sizeof(out));
 
 			}else if(strcmp(command, "get") == 0){
+            //Get filename
             sscanf(buf, "%s %s", command, filename);
+            //Get file stats on size to allocate
             stat(filename, &fileStat);
+
+            //Open and read file contents
             filehandle = open(filename, O_RDONLY);
 
+            //Verify file exists and is a file not a directory
             size = fileStat.st_size;
             if(filehandle == -1){
                 size = 0;
@@ -78,16 +87,20 @@ int main(int argc , char *argv[])
             if(S_ISDIR(fileStat.st_mode)){
                 size = 0;
             }
+            //Send size to client so that it can allocate memory
             send(client_fd, &size, sizeof(int), 0);
 
+            //If non zero size, then send the username and file to client.
             if(size){
                 send(client_fd, filename, sizeof(filename), 0);
                 sendfile(client_fd, filehandle, NULL, size);
             }
 
       }else if(strcmp(command, "quit") == 0){
+            //If client quits, send goodbye before closing
             write(client_fd , "Goodbye" , sizeof("Goodbye"));
       }else{
+            //If unknown command send error
         		write(client_fd, error, sizeof(error));
       }
 
